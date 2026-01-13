4abb82f241e933f51bd69b510e891f80fdfa1050
priyanshur0410 <priyanshur0410@gmail.com>
2026-01-13 21:33:29 +0530

    some changes

--- life_design_service/main.py
+++
@@ -1,6 +0,0 @@
-from fastapi import FastAPI
-from app.api.routes import router
-
-app = FastAPI(title="Life Design Backend Service")
-
-app.include_router(router)
---
+++ main.py
@@ -0,0 +1,8 @@
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+
+app = FastAPI()
+
+@app.get("/favicon.ico", include_in_schema=False)
+async def favicon():
+    return FileResponse("favicon.ico")
